7411afc6ee66975fa32bc886f0b06be6914f9c5e
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-17 22:32:17 -0300

    Feat: Estrutura visual - Criando radio button acessível

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import * as Dialog from '@radix-ui/react-dialog';
-
+import * as RadioGroup from '@radix-ui/react-radio-group';
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -9,6 +9,8 @@ export const Overlay = styled(Dialog.Overlay)`
   inset: 0;
   background: rgba(0, 0, 0, 0.75);
 `;
+
+
 export const Content = styled(Dialog.Content)`
   min-width: 32rem;
   border-radius: 6px;
@@ -18,6 +20,7 @@ export const Content = styled(Dialog.Content)`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
 
   form {
     margin-top: 2rem;
@@ -31,6 +34,7 @@ export const Content = styled(Dialog.Content)`
       background: ${props => props.theme["gray-900"]};
       color: ${props => props.theme["gray-300"]};
       padding: 1rem;
+
       &::placeholder {
         color: ${props => props.theme["gray-500"]};
       }
@@ -54,6 +58,8 @@ export const Content = styled(Dialog.Content)`
     }
   }
 `;
+
+
 export const CloseButton = styled(Dialog.Close)`
   position: absolute;
   background: transparent;
@@ -65,18 +71,20 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${props => props.theme["gray-500"]};
 `;
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
   margin-top: 0.5rem;
 `;
 
+
 interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome';
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
+
   background: ${props => props.theme["gray-700"]};
   padding: 1rem;
   display: flex;
@@ -91,5 +99,22 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
 
   svg {
     color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+  }
+
+
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${props => props.theme["gray-600"]};
+  }
+
+
+  &[data-state='checked'] {
+    color: ${props => props.theme.white};
+    background: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+
+
+    svg {
+      color: ${props => props.theme.white};
+    }
   }
 `;
