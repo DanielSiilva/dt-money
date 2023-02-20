d039fe731a70f9ad136f0dc35c5eb9fef5f83f87
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-19 22:27:50 -0300

    Feat: Conexão com a API - Criando nova transação

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as Dialog from '@radix-ui/react-dialog';
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { useContext } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
@@ -16,12 +18,15 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  
+  const { createTransaction } = useContext(TransactionsContext);
 
   const {
     control,
     register,
     handleSubmit,
-    formState: { isSubmitting }
+    formState: { isSubmitting },
+    reset
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
     defaultValues: {
@@ -30,20 +35,26 @@ export function NewTransactionModal() {
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log(data);
+    const { description, price, category, type } = data;
+
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    });
+
+    reset();
   }
 
   return (
     <Dialog.Portal>
       <Overlay />
       <Content>
-
         <Dialog.Title>Nova Transação</Dialog.Title>
         <CloseButton>
           <X size={24} />
         </CloseButton>
-
         <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
           <input
             type="text"
@@ -51,27 +62,22 @@ export function NewTransactionModal() {
             required
             {...register('description')}
           />
-
           <input
             type="number"
             placeholder="Preço"
             required
             {...register('price', { valueAsNumber: true })}
           />
-
           <input
             type="text"
             placeholder="Categoria"
             required
             {...register('category')}
           />
-
           <Controller
             control={control}
             name="type"
-            
             render={({ field }) => {
-
               return (
                 <TransactionType
                   onValueChange={field.onChange}
@@ -89,7 +95,6 @@ export function NewTransactionModal() {
               )
             }}
           />
-
           <button type="submit" disabled={isSubmitting}>
             Cadastrar
           </button>
