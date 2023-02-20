a3611ee3efb86ff771dc3613047dda47224adfa4
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-20 17:02:53 -0300

    Feat: Performance - Aplicando o useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,31 +1,34 @@
-
-import {useContextSelector} from 'use-context-selector'
+import { useMemo } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
   
-  const  transactions  = useContextSelector(TransactionsContext, (context)=>{
+  const transactions = useContextSelector(TransactionsContext, (context) => {
     return context.transactions
-  });
+  })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
+  const summary = useMemo(() => {
 
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-  return summary;
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
+
+  return summary
 }
